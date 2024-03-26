070d7ba39600bc9ecaeb1bbd3d3523cceeda932a
47491 <47491@gymnasium.ax>
2024-03-26 15:01:25 +0200

    kommentera och fixa bracketsen i mitten

--- volleyboll_slutspel/src/App.vue
+++ volleyboll_slutspel/src/App.vue
@@ -1,17 +1,21 @@
 
 <script>
+// Importera vue-tournament-bracket
 import VueTournamentBracket from 'vue-tournament-bracket';
 
 export default {
   components: {
+     // Registrera VueTournamentBracket
     VueTournamentBracket,
   },
   data() {
     return {
+       // Data om turnenringaran för players/teams i bracketsen
       rounds: [
       {
           games: [
             {
+              // Datan inom bracketsen
               player1: { id: "1", name: "Competitor 1", winner: false },
               player2: { id: "4", name: "Competitor 4", winner: true },
             },
@@ -59,11 +63,24 @@ export default {
 
 
 <template>
-  <div>
-    <vue-tournament-bracket :rounds="rounds">
-      <template v-slot:player="{ player }">
-        {{ player.name }}
-      </template>
-    </vue-tournament-bracket>
+  <div class="container">
+      <!-- visa vad som är i bracketerna-->
+      <vue-tournament-bracket :rounds="rounds">
+        <!-- Visa spelarnas namn -->
+        <template v-slot:player="{ player }">
+          {{ player.name }}
+        </template>
+      </vue-tournament-bracket>
   </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+}
+
+.vtb-wrapper {
+  position: relative; 
+  left: 50%;
+}
+</style>
