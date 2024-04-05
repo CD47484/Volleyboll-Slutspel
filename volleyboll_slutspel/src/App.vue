5e835346ab02e33e3f2f627e3f4545d981ec9d6c
47491 <47491@gymnasium.ax>
2024-04-05 10:35:14 +0300

    phone standing

--- volleyboll_slutspel/src/App.vue
+++ volleyboll_slutspel/src/App.vue
@@ -15,7 +15,7 @@ export default {
           games: [
             {
               // Datan inom bracketsen
-              player1: { id: "2", name: "Deltagare 2", winner: "not-started" },
+              player1: { id: "2", name: "Deltagare 2", winner: true },
               player2: { id: "4", name: "Deltagare 4", winner: false },
             },
             {
@@ -133,6 +133,12 @@ export default {
       </div>
     </template>
   </vue-tournament-bracket>
+
+  <div class="phone-container">
+  <img class="phone-img" src="https://cdn-icons-png.freepik.com/512/68/68737.png" alt="turn the phone">
+  </div>
+
+
 </template>
 
 
@@ -146,7 +152,7 @@ export default {
   left:0%;
   right:0%;
   height:3.5em;
-  z-index:100;
+  z-index:200;
 }
 
 .hamburger{
@@ -205,7 +211,52 @@ export default {
 .vtb-item-players .defeated {
   background-color: red !important;
 }
+
+.phone-img {
+  display: none;
+  }
+
 @media only screen and (max-height:400px){
 
 }
+
+@media only screen and (max-width:450px){
+  .phone-img {
+    display: flex;
+    height: 40%;
+    width: auto;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 150; 
+  }
+
+  .phone-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(0, 128, 0); 
+    z-index: 100; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 149;
+  }
+
+  .nav {
+  background-color: blue;
+  position:fixed;
+  top:0%;
+  left:0%;
+  right:0%;
+  height:4.5em;
+  z-index:200;
+}
+
+
+}
+
 </style>
